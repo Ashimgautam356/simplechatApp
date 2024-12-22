cecbe1246dfeebc6c9bf382498787792cb17f1cc
Ashim Gautam <computer77356@gmail.com>
2024-12-22 21:36:45 +0545

    learned redux-toolkit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { useAppSelector } from './store/hooks'
+import { useDispatch } from 'react-redux'
+import { increment, decrement } from './store/slices/counter'
 
 export const App = () => {
+  const count = useAppSelector(state => state.counter)
+  const dispatch = useDispatch()
   return (
-    <div className='text-green-600'>App</div>
+    <div className='flex justify-center items-center h-screen w-screen bg-gray-600'>
+        <div className='p-8 border border-white rounded-lg text-white flex flex-col'>
+            <h1 className='p-2'>{count}</h1>
+            <button className='p-2 border border-white rounded-md my-4' onClick={()=>dispatch(increment())}>increase</button>
+            <button className='p-2 border border-white rounded-md' onClick={()=> dispatch(decrement())}>decrease</button>
+        </div>
+    </div>
   )
 }
